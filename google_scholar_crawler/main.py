2137a45d4c0e7e892330edd065112c18e94b5a7b
j-dut.github.io <zzupchd@gmail.com>
2024-01-06 14:15:36 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -21,3 +21,19 @@ shieldio_data = {
 }
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
+
+shieldio_data_hindex = {
+  "schemaVersion": 1,
+  "label": "h-index",
+  "message": f"{author['hindex']}",
+}
+with open(f'results/gs_data_shieldsio_hindex.json', 'w') as outfile:
+    json.dump(shieldio_data_hindex, outfile, ensure_ascii=False)
+
+shieldio_data_i10 = {
+  "schemaVersion": 1,
+  "label": "i10-index",
+  "message": f"{author['i10index']}",
+}
+with open(f'results/gs_data_shieldsio_i10.json', 'w') as outfile:
+    json.dump(shieldio_data_i10, outfile, ensure_ascii=False)
